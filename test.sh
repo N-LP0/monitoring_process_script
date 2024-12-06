34b8737c00fbeb57e3a34f338d3c42b165ddddb6
N-LP0 <138440804+N-LP0@users.noreply.github.com>
2024-12-06 23:29:31 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,13 +1,44 @@
-SHELL=/bin/bah
-PATH=/sbin/bin:/usr/sbin:/usr/bin
-
 #!/bin/bash
 
-service nginx status > output.txt
+MYSITE="https://www.google.com"
+PROCESS="sshd"
+MYAPI="https://www.google.com/myapi"
+LOGFILE="/var/log/monitoring.log"
+TIMEWAIT=60
+DEBUG=false
 
-if grep -q "running" output.txt; then
-echo "The nginx service is running"
-else
-service nginx start
-echo "The nginx service was stopped, errored, or inactive. The service has been started." | mail -s "The nginx service was stopped." ##@##.org
-fi
+
+checksite() {
+  local RESPONSE=$(curl -s --head  --request GET ${1} | grep "HTTP/2 200" | awk '{print $2}')
+  if [[ "${RESPONSE}" -ne 200 ]]; then 
+   monitoring_log "my site ${1} is DOWN"
+  fi
+}
+
+api(){
+  curl -X POST -d "process=${1}" $2
+}	
+
+log() {
+  echo $1 
+}
+
+monitoring_log() {
+  echo $1 >> $LOGFILE
+  if [[ "${DEBUG}" ]]; then
+    log "${1}"
+  fi 
+}
+
+while true; do
+  PID=$(pgrep -x $PROCESS)
+  if [[ "${TMPPID}" -ne "${PID}" ]]; then
+    monitoring_log "${PID} ${PROCESS} process was restarted"
+  fi
+  if [[ -v "${PID}" ]]; then
+    api $PROCESS $MYAPI 
+  fi	  
+  TMPPID=$PID
+  checksite $MYSITE
+  sleep $TIMEWAIT
+done
